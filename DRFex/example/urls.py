35de9d5c3fcfc09bb92bdd329574c72e40e6eb9f
Zion-min <sioniasak@naver.com>
2023-05-26 14:26:15 +0900

    [Feat] Rest API 서버 만들기

---
+++ DRFex/example/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from .views import HelloAPI, bookAPI, booksAPI, BookAPI, BooksAPI, BooksAPIMixins, BookAPIMixins, BooksAPIGenerics, BookAPIGenerics, BookViewSet
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register('books', BookViewSet)
+
+urlpatterns = [
+    path("hello/", HelloAPI),
+    path("fbv/books/", booksAPI), 
+    path("fbv/book/<int:bid>/", bookAPI), 
+    path("cbv/books/", BooksAPI.as_view()),
+    path('cbv/book/<int:bid>', BookAPI.as_view()),
+    path("mixin/books/", BooksAPIMixins.as_view()),
+    path("mixin/book/<int:bid>/", BookAPIMixins.as_view()),
+    path("generics/books/", BooksAPIGenerics.as_view()),
+    path("generics/book/<int:bid>/", BookAPIGenerics.as_view()),
+]
+
+urlpatterns = router.urls
+
